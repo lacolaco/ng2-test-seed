64cb23a51df005b4b2205f604c5c08a0950f79c1
laco0416 <laco0416+github@gmail.com>
2016-10-25 00:34:33 +0900

    step-1: simple karma testing

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+module.exports = () => {
+    return {
+        entry: {
+            main: './src/main.ts'
+        },
+        output: {
+            path: './dist',
+            filename: '[name].bundle.js'
+        },
+        resolve: {
+            extensions: ['.js', '.ts', '.html']
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.ts$/,
+                    loaders: [
+                        'awesome-typescript-loader',
+                        'angular2-template-loader'
+                    ]
+                },
+                {
+                    test: /\.html$/,
+                    loader: 'raw'
+                }
+            ]
+        },
+        devtool: 'inline-source-map'
+    };
+};
