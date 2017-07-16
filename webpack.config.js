d6c4b1c10f2dadf0a5907dcd7ebb906c0915e360
Arthur Melo <contact@arthurmelo.com>
2017-07-15 21:10:13 -0300

    Added missing -loader postfix

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ module.exports = () => {
                 },
                 {
                     test: /\.html$/,
-                    loader: 'raw'
+                    loader: 'raw-loader'
                 }
             ]
         },
